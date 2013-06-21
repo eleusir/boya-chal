b50df14cfa1e2c877e94c5a646a82de2288d105a
Eleu Isis <io.org>
2013-06-21 10:01:59 -0400

    arrays work.

--- game.c
+++ game.c
@@ -1,7 +1,28 @@
-#include <stdio.h>
+#include <curses.h>
+
+void poofunction()
+{
+     unsigned int fbf[5][5];
+
+     int i, j;
+
+     for (i = 0; i < 5; i++) {
+          for (j = 0; j < 5; j++) {
+              fbf[i][j] = i*5+j;
+          }
+     }
+     for (i = 0; i < 5; i++) {
+          for (j = 0; j < 5; j++) {
+              printf("[%d, %d] = %d\t", i, j, fbf[i][j]);
+          }
+     printf("\n");
+     }
+     return;
+}
 
 int main() 
 {
-    return 1;
+     poofunction();
+     return 0;
 }
 
