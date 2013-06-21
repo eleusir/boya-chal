8afa84635657123ed899ec765281488dd77739a4
Eleu Isis <io.org>
2013-06-21 11:36:45 -0400

    ncurses works. prints array representing game board. press escape to exit.

--- game.c
+++ game.c
@@ -1,5 +1,47 @@
 #include <curses.h>
 
+void printarray(unsigned int fbf[5][5]) 
+{
+     int i, j;
+
+     for (i = 0; i < 5; i++) {
+          for (j = 0; j < 5; j++) {
+              printf("[%d, %d] = %d\t", i, j, fbf[i][j]);
+          }
+     printf("\n");
+     }
+     return;
+}
+
+void ncursesprintarray(unsigned int fbf[5][5])
+{
+     int i, j;
+     int ch;
+
+     initscr();
+     noecho();
+     raw();
+
+     printw("(X)-(o)-(o)-(o)-(X)\n");
+     printw(" | \\ | / | \\ | / | \n");
+     printw("(o)-(o)-(o)-(o)-(o)\n");
+     printw(" | / | \\ | / | \\ | \n");
+     printw("(o)-(o)-( )-(o)-(o)\n");
+     printw(" | \\ | / | \\ | / | \n");
+     printw("(o)-(o)-(o)-(o)-(o)\n");
+     printw(" | / | \\ | / | \\ | \n");
+     printw("(X)-(o)-(o)-(o)-(X)\n");
+
+     while (ch != 27) {
+          refresh();
+          ch = getch();
+          printw("%d\n", ch);
+     }
+     
+     endwin();
+     return;
+}
+
 void poofunction()
 {
      unsigned int fbf[5][5];
@@ -11,12 +53,9 @@ void poofunction()
               fbf[i][j] = i*5+j;
           }
      }
-     for (i = 0; i < 5; i++) {
-          for (j = 0; j < 5; j++) {
-              printf("[%d, %d] = %d\t", i, j, fbf[i][j]);
-          }
-     printf("\n");
-     }
+
+     ncursesprintarray(fbf);
+
      return;
 }
 
